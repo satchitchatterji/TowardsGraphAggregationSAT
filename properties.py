b685db4c96b2008d7909e6cf82af63ef49a9adaa
Farrukh Baratov <farrukhsbaratov@gmail.com>
2023-12-03 18:25:11 +0100

    all properties encoded

--- properties.py
+++ properties.py
@@ -1,97 +1,115 @@
+from utils import (
+    allVertices,
+    allVoters,
+    allGraphs,
+    allEdges,
+    posEdgeLiteral, # (node x, node y)
+    negEdgeLiteral, # (node x, node y)
+)
+
 def cnfReflexivity(V):
     """Reflexivity property
-
-    Args:
-        V (List): List of all vertices
 
     Returns:
         cnf: List of tuples containing clauses for reflexivity CNF
     """
     cnf = []
 
+    # edge from x to x for each note
+    for x in allVertices():
+        cnf.append((posEdgeLiteral(x,x)))
 
     return cnf
 
 def cnfIrreflexivity(V):
     """Irreflexivity property
 
-    Args:
-        V (List): List of all vertices
-
     Returns:
         cnf: List of tuples containing clauses for irreflexivity CNF
     """
     cnf = []
 
+    for x in allVertices():
+        cnf.append((negEdgeLiteral(x,x)))
 
     return cnf
 
 def cnfTransitivity(V):
     """Transitivity property
 
-    Args:
-        V (List): List of all vertices
-
     Returns:
         cnf: List of tuples containing clauses for transitivity CNF
     """
     cnf = []
 
+    for x in allVertices():
+        for y in allVertices():
+            for z in allVertices():
+                cnf.append((negEdgeLiteral(x,y),
+                            negEdgeLiteral(y,z),
+                            posEdgeLiteral(x,z)))
 
     return cnf
 
 def cnfCompleteness(V):
     """Completeness property
 
-    Args:
-        V (List): List of all vertices
-
     Returns:
         cnf: List of tuples containing clauses for completeness CNF
     """
     cnf = []
-
-
+    
+    for x in allVertices():
+        for y in allVertices():
+            if x != y:
+                cnf.append((posEdgeLiteral(x,y), posEdgeLiteral(y,x)))
+    
     return cnf
 
 def cnfConnectedness(V):
     """Reflexivity property
-
-    Args:
-        V (List): List of all vertices
 
     Returns:
         cnf: List of tuples containing clauses for connectedness CNF
     """
     cnf = []
 
+    for x in allVertices():
+        for y in allVertices():
+            for z in allVertices():
+                cnf.append((negEdgeLiteral(x,y), negEdgeLiteral(x,z), posEdgeLiteral(y,z)))
+                cnf.append((negEdgeLiteral(x,y), negEdgeLiteral(x,z), posEdgeLiteral(z,y)))
 
     return cnf
 
 def cnfNontriviality(V):
     """Reflexivity property
 
-    Args:
-        V (List): List of all vertices
-
     Returns:
         cnf: List of tuples containing clauses for nontriviality CNF
     """
     cnf = []
 
+    clause = []
+    for x in allVertices():
+        for y in allVertices():
+            clause.append(posEdgeLiteral(x,y))
+
+    cnf.append(tuple(posEdgeLiteral))
 
     return cnf
 
 def cnfSeriality(V):
     """Seriality property
 
-    Args:
-        V (List): List of all vertices
-
     Returns:
         cnf: List of tuples containing clauses for seriality CNF
     """
     cnf = []
 
-
+    for x in allVertices():
+        clause = []
+        for y in allVertices():
+            clause.append(posEdgeLiteral(x,y))
+        cnf.append(tuple(clause))
     return cnf
