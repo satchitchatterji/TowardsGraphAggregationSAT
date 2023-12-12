d7caf2c30b2491494b0616e35528ff49f7986e8e
Satchit Chatterji <satchit.chatterji@gmail.com>
2023-12-12 09:10:27 +0100

    Added unanimity and fixed groundedness

--- axioms.py
+++ axioms.py
@@ -54,6 +54,25 @@ def unanimity():
 	return cnf
 
 
+
+def unanimity_imp():
+	cnf = []
+
+	for E in allProfiles():
+		# for winning origin
+		for xwin in allVertices():
+			# for winning target
+			for ywin in allVertices():
+				graphs_in_E = profileIntToProfile(E)
+				for graph_int in graphs_in_E:
+					if (xwin,ywin) not in get_graph(graph_int, config.v):
+						# cnf.append((posLiteral(E,xwin,ywin),))
+					# else:
+						cnf.append((negLiteral(E,xwin,ywin),))
+					break
+
+	return cnf
+
 def grounded():
 	cnf = []
 	exp_c =  config.r*config.v*config.v*config.n
@@ -76,12 +95,14 @@ def grounded_imp():
 			# for winning target
 			for ywin in allVertices():
 				graphs_in_E = profileIntToProfile(E)
+				edge_exists_in_any_player = False
 				for graph_int in graphs_in_E:
-					if (xwin,ywin) not in get_graph(graph_int, config.v):
-						# cnf.append((posLiteral(E,xwin,ywin),))
-					# else:
-						cnf.append((negLiteral(E,xwin,ywin),))
-					break
+					if (xwin,ywin) in get_graph(graph_int, config.v):
+						edge_exists_in_any_player = True
+						break
+
+				if not edge_exists_in_any_player:
+					cnf.append((negLiteral(E,xwin,ywin),))
 
 	return cnf
 
