4ae55ebdd30ca7169610ab5693f1abc642f78bea
Farrukh Baratov <farrukhsbaratov@gmail.com>
2023-12-21 23:09:18 +0100

    expanded tests - everything seems to work

--- testgraphs.py
+++ testgraphs.py
@@ -12,27 +12,23 @@ if __name__ == "__main__":
     graphs = generate_graph_subsets([cnfIrreflexivity, cnfTransitivity, cnfCompleteness])
     config.update_graphs(graphs)
 
+    
+
     match = 0
     mismatch = 0
     duplicates = 0
 
-    all_profiles = []
+    all_graphs = []
+    print(config.graphs)
+    for g in config.graphs:
+        g_dec = get_graph(g, config.v)
+        g_dec.sort() # since this is order-invariant anyway, sort to alter test for duplicates
 
-    for E in allProfiles():
-        E_dec = profileIntToProfile(E)
-        E_rec = profileToProfileInt(E_dec)
-
-        if E == E_rec:
-            match += 1
-        else:
-            mismatch += 1
-            print(E, "\n\n", E_rec)
-
-        if E_dec in all_profiles:
+        print(g_dec)
+        if g_dec in all_graphs:
             duplicates += 1
-        all_profiles.append(E_dec)
 
+        all_graphs.append(g_dec)
 
-
-    print(f"{match} MATCHES, {mismatch} MISMATCHES")
+    #print(f"{match} MATCHES, {mismatch} MISMATCHES")
     print(f"{duplicates} DECODED DUPLICATES")
