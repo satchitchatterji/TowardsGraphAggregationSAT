86d78bde868ce74fd879a87e93c576d42854d8c0
Farrukh Baratov <farrukhsbaratov@gmail.com>
2024-03-19 14:54:10 +0100

    working on weak arrow demo

--- asp/generate_profiles.py
+++ asp/generate_profiles.py
@@ -1,10 +1,11 @@
 from itertools import permutations
 
 
-def generate_profiles(n_agents=3,n_nodes=3):
-    n_graphs=13
-    asp = """"""
+def generate_profiles(n_agents=3,n_graphs=13):
+    #n_graphs=13
     
     perms = list(permutations(range(1,n_graphs+1),n_agents))
     for i in range(1,n_graphs+1):
         perms += list([i] * n_agents)
+    
+    return perms
